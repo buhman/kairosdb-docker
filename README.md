82919ccf38d05aa51fa8d149b93b19263993588e
Zack Buhman <zack@buhman.org>
2017-08-13 01:25:42 -0500

    run.sh: use dockerize for cassandra timeout and template building
    This also adds a new `CASSANDRA_WAIT_TIMEOUT` variable

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ### `CASSANDRA_CQL_HOST_LIST`
 
 List is in the form: `1.1.1.1,1.1.1.2`.
+
+Default: `cassandra:9042`
+
+### `CASSANDRA_WAIT_TIMEOUT`
+
+Wait for cassandra to become available; useful for docker-compose, but should
+probably be lowered for production usage.
+
+Default: `30s`
--- run.sh
+++ run.sh
@@ -3,7 +3,10 @@
 set -e
 
 export CASSANDRA_CQL_HOST_LIST=${CASSANDRA_CQL_HOST_LIST:-"cassandra:9042"}
+export CASSANDRA_TIMEOUT=${CASSANDRA_TIMEOUT:-"30s"}
 
-envsubst < $KAIROSDB_HOME/conf/kairosdb.properties.env > $KAIROSDB_HOME/conf/kairosdb.properties
-
-exec $KAIROSDB_HOME/bin/kairosdb.sh run
+exec dockerize \
+     -template $KAIROSDB_HOME/conf/kairosdb.properties.tmpl:$KAIROSDB_HOME/conf/kairosdb.properties \
+     -wait tcp://${CASSANDRA_CQL_HOST_LIST%%,*} \
+     -timeout $CASSANDRA_TIMEOUT \
+     $KAIROSDB_HOME/bin/kairosdb.sh run
